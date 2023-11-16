c5bd84f6f8b223864bd1d612d4cdb6d31f6fd2b5
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2023-11-16 00:18:25 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateAndDisplayExcuse();
+
+  function generateAndDisplayExcuse() {
+    let whoList = ["The dog", "My neighbor", "His turtle", "My bird"];
+    let actionList = ["ate", "peed", "crushed", "broke"];
+    let whatList = ["my homework", "the keys", "the car"];
+    let whenList = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let randomWho = Math.floor(Math.random() * whoList.length);
+    let randomAction = Math.floor(Math.random() * actionList.length);
+    let randomWhat = Math.floor(Math.random() * whatList.length);
+    let randomWhen = Math.floor(Math.random() * whenList.length);
+
+    let randomExcuse =
+      whoList[randomWho] +
+      " " +
+      actionList[randomAction] +
+      " " +
+      whatList[randomWhat] +
+      " " +
+      whenList[randomWhen];
+
+    document.querySelector("#excuse").innerHTML = randomExcuse;
+
+    return randomExcuse;
+  }
 };
